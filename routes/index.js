db5640a7a73d928a6fa52154c5d2ec0aefb62bca
younger50 <pt54088@gmail.com>
2015-05-28 21:15:47 +0800

    [Test] Add server time at index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  date = new Date();
+  res.render('index', { 
+  	title: 'Express',
+  	timenow: date.toDateString() });
 });
 
 module.exports = router;
