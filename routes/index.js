860b513f3d4af7bd7c7453431ec154d507e4696f
younger50 <pt54088@gmail.com>
2015-06-02 16:25:47 +0800

    [Test] Edit title and time var render to index page

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   date = new Date();
   res.render('index', { 
-  	title: 'Express',
-  	timenow: date.toDateString() });
+  	title: 'iBobeeU',
+  	timenow: date.toString() });
 });
 
 module.exports = router;
