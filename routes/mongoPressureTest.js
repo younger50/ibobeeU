33b2351dc86017faa41ccf290926c2985f4b82e3
sandy <sandy30716@gmail.com>
2015-06-10 20:49:34 +0800

    [Test] test for pressure test

---
+++ routes/mongoPressureTest.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+// acceess mongolab by REST API
+var request = require('request');
+var api_key = "YXbD6RBoICYSyTacuCIYQ5cbk0eD4TsX";
+var api_raw_url = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest";
+var api_url = api_raw_url+"?apiKey="+api_key;
+var router = express.Router();
+
+
+// POST add many users given data into db for test by Sandy 2015/06/10
+router.post('/db/addMany', function (req, res){
+  
+  for(var i=0;i<10;i++){
+	  var date = new Date();
+	  var timestamp = date.getTime();
+	  var words = "test"+i;
+	  console.log(words);
+	  // injection char filter
+	  //words = words.replace(/['"]/g, "");
+	  //console.log(words);
+	  // requset POST to mlab API
+	  request( 
+		{ 
+		  url: inst_api_url, 
+		  method: "POST", 
+		  json: {"words":words}
+		}, 
+		function (error, response, body) {
+		console.log(body);
+		res.send(body);
+	  });
+  }
+});
+
+module.exports = router;
