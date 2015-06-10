c76e3c5adb48420f1caabdb95c96a6b476434dd4
younger50 <pt54088@gmail.com>
2015-06-10 17:34:38 +0800

    [Stable] Can insert to mongolab through api

--- routes/dbmongolab.js
+++ routes/dbmongolab.js
@@ -1,8 +1,10 @@
 var express = require('express');
+
+// acceess mongolab by REST API
 var request = require('request');
-
-var get_sample = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest?apiKey=YXbD6RBoICYSyTacuCIYQ5cbk0eD4TsX"
-
+var api_key = "YXbD6RBoICYSyTacuCIYQ5cbk0eD4TsX";
+var find_api_url = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest?apiKey="+api_key;
+var inst_api_url = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest?apiKey="+api_key;
 var router = express.Router();
 
 // GET db homepage
@@ -15,13 +17,40 @@ router.get('/', function (req, res, next) {
 
 // GET db show all
 router.get('/db', function (req, res, next) {
-  //
-  request( get_sample, function (error, response, body) {
+  // through mongolab REST API
+  request( find_api_url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body) // Show the HTML for the Google homepage.
       res.send(body);
     }
   });
+});
+
+// POST add user given data into db
+router.post('/db/add', function (req, res){
+  var date = new Date();
+  var timestamp = date.getTime();
+  var words = req.body.words;
+  console.log(words);
+  // injection char filter
+  words = words.replace(/['"]/g, "");
+  console.log(words);
+  // requset POST to mlab API
+  request( 
+    { 
+      url: inst_api_url, 
+      method: "POST", 
+      json: {"words":words}
+    }, 
+    function (error, response, body) {
+    console.log(body);
+    res.send(body);
+  });
+});
+
+// POST delete user given data from db
+router.post('/db/del', function(request, response){
+
 });
 
 module.exports = router;
