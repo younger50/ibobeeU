223a4a5f032cc70497d1214ff8f0592871912ad4
younger <pt54088@gmail.com>
2015-06-10 23:02:24 +0800

    [Resetdb] mongolab url and key set to new host

--- routes/dbmongolab.js
+++ routes/dbmongolab.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 // acceess mongolab by REST API
 var request = require('request');
-var api_key = "YXbD6RBoICYSyTacuCIYQ5cbk0eD4TsX";
-var api_raw_url = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest";
+var api_key = "sazUWtXlKYRLcswVw_i6BYQV8Ia6tyE5";
+var api_raw_url = "https://api.mongolab.com/api/1/databases/ibbu-mongo1/collections/dbtest";
 var api_url = api_raw_url+"?apiKey="+api_key;
 var router = express.Router();
 
