eb6341939c71a18be80167153caea49c1facf27b
younger50 <pt54088@gmail.com>
2015-06-10 20:03:37 +0800

    [Stable] Can delete document from mongolab

--- routes/dbmongolab.js
+++ routes/dbmongolab.js
@@ -3,8 +3,8 @@ var express = require('express');
 // acceess mongolab by REST API
 var request = require('request');
 var api_key = "YXbD6RBoICYSyTacuCIYQ5cbk0eD4TsX";
-var find_api_url = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest?apiKey="+api_key;
-var inst_api_url = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest?apiKey="+api_key;
+var api_raw_url = "https://api.mongolab.com/api/1/databases/ibu-test1/collections/dbtest";
+var api_url = api_raw_url+"?apiKey="+api_key;
 var router = express.Router();
 
 // GET db homepage
@@ -15,42 +15,63 @@ router.get('/', function (req, res, next) {
   });
 });
 
-// GET db show all
+// show all data in db mongolab
 router.get('/db', function (req, res, next) {
   // through mongolab REST API
-  request( find_api_url, function (error, response, body) {
+  request( api_url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
+      console.log(body);
       res.send(body);
     }
   });
 });
 
-// POST add user given data into db
+// add user given data into db
 router.post('/db/add', function (req, res){
-  var date = new Date();
-  var timestamp = date.getTime();
-  var words = req.body.words;
-  console.log(words);
   // injection char filter
+  var words = req.body.words;  
   words = words.replace(/['"]/g, "");
   console.log(words);
-  // requset POST to mlab API
+  // requset POST to mlab API as data insert
   request( 
     { 
-      url: inst_api_url, 
+      url: api_url, 
       method: "POST", 
       json: {"words":words}
     }, 
     function (error, response, body) {
-    console.log(body);
-    res.send(body);
+      console.log(body);
+      res.send(body);
   });
 });
 
-// POST delete user given data from db
-router.post('/db/del', function(request, response){
-
+// delete user given data from db
+router.post('/db/del', function (req, res){
+  // injection char filter
+  var words = req.body.words;  
+  words = words.replace(/['"]/g, "");
+  console.log(words);
+  // requset GET to mlab API as data searc
+  url = api_raw_url+"?q="+JSON.stringify({"words":words})+"&apiKey="+api_key;
+  //console.log(url);
+  request( url, 
+    function (error, response, body) {
+      //console.log(body);
+      body = JSON.parse(body);
+      for (var i = 0; i < body.length; i++) {
+        var data = body[i];
+        // delete data by specific id
+        url = api_raw_url+"/"+data["_id"]["\$oid"]+"?apiKey="+api_key;
+        request( 
+        {
+          url:url,
+          method:"DELETE"
+        }, function (error, response, body) {
+          console.log(body);
+        });
+      };
+      res.send(body);
+  });
 });
 
 module.exports = router;
