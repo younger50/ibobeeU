1132f316b166d3abf25466b098c932ce04ba77d6
younger50 <pt54088@gmail.com>
2015-06-11 21:56:16 +0800

    [Crawler] add festival insert function

--- utilities/crawler_dbadd.js
+++ utilities/crawler_dbadd.js
@@ -6,7 +6,6 @@ var request = require('request');
 var api_key = "sazUWtXlKYRLcswVw_i6BYQV8Ia6tyE5";
 var api_raw_url = "https://api.mongolab.com/api/1/databases/ibbu-mongo1/collections/";
 var api_url = api_raw_url+"dbtest"+"?apiKey="+api_key;
-var temple_url =  api_raw_url+"dbtemples"+"?apiKey="+api_key;
 
 // json load and string porcessing
 var fs = require('fs');
@@ -18,6 +17,7 @@ var lines;
 
 // temple load test
 function dbinsert_temple( address, area, city, deity, latitude, longitude, name, religion, temple_id){
+  var temple_url =  api_raw_url+"dbtemples"+"?apiKey="+api_key;
   request( 
     { 
       url: temple_url, 
@@ -78,43 +78,81 @@ function temple_json_load(filename){
 		lines = data.split("\n");
 		//use recursive callback instead of loop to guarentee dealy happen and variable passing
 		temple_add_callback( lines, 0);
-		/*
-		for ( i = lines.length - 1; i >= 0; i--) {
-			var line = lines[i];
-			line = line.replace(/NumberInt\(/g," ");
-			line = line.replace(/\)/g," ");
-			console.log("===============");
-			console.log(line);			
-			if(line!=""){
-				// load as json and add to remote mongolab db
-				var doct = JSON.parse("["+line+"]");
-				console.log(doct[0]["_id"]);
-				setTimeout( function(){
-					var doc = doct;
-					dbinsert_temple(
-				      	address=doc[0].address,
-				      	area=doc[0].area,
-				      	city=doc[0].city,
-				      	deity=doc[0].deity,
-				      	latitude=doc[0].latitude,
-				      	longitude=doc[0].longitude,
-				      	name=doc[0].name,
-				      	religion=doc[0].religion,
-				      	temple_id=doc[0].temple_id
-					);
-				}, 1+i*10); // delay add to avoid db connection limits
-
-			}
-			console.log("---------------");
-		};
-		console.log(lines.length);
-		*/
 	});
 
 }
 
 // load
-temple_json_load("temple.json");
+//temple_json_load("temple.json");
+
+// festival
+// temple load test
+function dbinsert_festival( area, city, date, detail, group, id, name){
+  var festival_url =  api_raw_url+"dbfestival"+"?apiKey="+api_key;
+  request( 
+    { 
+      url: festival_url, 
+      method: "POST", 
+      json: 
+      {
+      	"area": area,
+      	"city": city,
+      	"date": date,
+      	"detail": detail,
+      	"group": group,
+      	"id": id,
+      	"name": name
+      }
+    }, 
+    function (error, response, body) {
+      console.log(body);
+  });
+}
+
+//
+function festival_add_callback( lines, i){
+	if( i<lines.length ){
+		var line = lines[i];
+		//console.log(""+i+":"+lines[i]);
+		line = line.replace(/NumberInt\(/g," ");
+		line = line.replace(/\)/g," ");
+		console.log("===============");
+		if(line!=""){
+			var doc = JSON.parse("["+line+"]");
+			console.log(doc);
+			dbinsert_festival(
+				area=doc[0].area,
+				city=doc[0].city,
+				date=doc[0].date,
+				detail=doc[0].detail,
+				group=doc[0].group,
+				id=doc[0].id,
+				name=doc[0].name
+			);
+		}
+		console.log("---------------");
+		setTimeout( function (){ festival_add_callback( lines, i+1)}, 100); // delay add to avoid db connection limits
+		return;
+	}
+	else{
+		console.log(lines.length);
+		return;
+	}
+}
+
+//
+function festival_json_load(filename){
+	fs.readFile( filename, 'utf8', function (err, data){
+		//console.log(data);
+		lines = data.split("\n");
+		//use recursive callback instead of loop to guarentee dealy happen and variable passing
+		festival_add_callback( lines, 0);
+	});
+
+}
+
+// load
+festival_json_load("festival.json");
 
 /*
 //solve CR/LF parsing problem
