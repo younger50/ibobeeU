f4eb63d7f09396d5c57737fbf8934256d4565b71
sandy wu <sandy30716@gmail.com>
2015-06-12 22:40:52 +0800

    [Add] provide a function to easily display street view by coordinate

---
+++ public/javascripts/streetview.js
@@ -0,0 +1,43 @@
+/*
+usage: 
+1.create a div whose id is named 'pano'
+2.call getStreetView(lat,lng) example: showStreetView(25.11617,121.8414007);
+Example: in other file
+    <script src="./streetview.js"></script>
+    <script>showStreetView(25.11617,121.8414007);</script>
+    <div id="pano" style="width: 100%; height: 100%;float:left"></div>
+*/
+var lat;
+var lng;
+function initialize() {
+  var location = new google.maps.LatLng(lat, lng);
+
+  var panoramaOptions = {
+    position: location,
+    pov: {
+      heading: 34,
+      pitch: 10
+    }
+  };
+  var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+  var map = new google.maps.Map();
+  map.setStreetView(panorama);
+}
+function showStreetView(latitude,longitude){
+  lat=latitude;
+  lng=longitude;
+  google.maps.event.addDomListener(window, 'load', initialize);
+}
+function loadScript() {
+  var script   = document.createElement('script');
+  var script2  = document.createElement('script2');
+  script.type  = 'text/javascript';
+  script2.type = 'text/javascript';
+  script.src   = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +'&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+  document.body.appendChild(script2);
+}
+window.onload = loadScript;
+
+
+
