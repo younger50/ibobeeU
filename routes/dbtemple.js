a2320975a65142701a8efed8f178c14ba0674d23
younger50 <pt54088@gmail.com>
2015-06-14 19:57:32 +0800

    [Add] db query on node js server side after getting raw data from mongolab

--- routes/dbtemple.js
+++ routes/dbtemple.js
@@ -19,7 +19,7 @@ router.post('/findadrs', function (req, res, next) {
   //console.log(req.body);
   words = req.body.words;
   q_string = "{\"address\":{$regex:\".*"+words+".*\"}}";
-  q_url = api_raw_url+"?q="+q_string+"&apiKey="+api_key;
+  //q_url = api_raw_url+"?q="+q_string+"&apiKey="+api_key;
   q_url = api_raw_url+"?apiKey="+api_key;
   console.log(q_url);
   // query monglab through url api
@@ -29,8 +29,24 @@ router.post('/findadrs', function (req, res, next) {
     }, 
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
-      console.log(body);
-      res.send(body);
+        // do query / parsing with node js
+        body = JSON.parse(body);
+        for (var i = 0; i < body.length; i++) {
+          item = body[i];
+          //console.log(item);
+          if(item.address){
+            if(item.address.indexOf(words)>-1){
+              console.log(item);
+            }
+            else{
+              body.splice(i,1);
+              i--;
+            }
+          }
+        }
+        body = JSON.stringify(body);
+        console.log(body);
+        res.send(body);
       }
       else{
         console.log("error");
@@ -45,7 +61,7 @@ router.post('/findkey', function (req, res, next) {
   //console.log(req.body);
   words = req.body.words;
   q_string = "{\"name\":{$regex:\".*"+words+".*\"}}";
-  q_url = api_raw_url+"?q="+q_string+"&l=10"+"&apiKey="+api_key;
+  //q_url = api_raw_url+"?q="+q_string+"&l=10"+"&apiKey="+api_key;
   q_url = api_raw_url+"?apiKey="+api_key;
   console.log(q_url);
   // query monglab through url api
@@ -54,8 +70,24 @@ router.post('/findkey', function (req, res, next) {
     }, 
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
-      console.log(body);
-      res.send(body);
+        // do query / parsing with node js
+        body = JSON.parse(body);
+        for (var i = 0; i < body.length; i++) {
+          item = body[i];
+          //console.log(item);
+          if(item.address){
+            if(item.address.indexOf(words)>-1){
+              console.log(item);
+            }
+            else{
+              body.splice(i,1);
+              i--;
+            }
+          }
+        }
+        body = JSON.stringify(body);
+        console.log(body);
+        res.send(body);
       }
       else{
         console.log("error");
