6e2da06106a41e3b584a20faf4c94a38f91f0cef
younger50 <pt54088@gmail.com>
2015-06-14 17:34:48 +0800

    [Test] query temple db with regex

--- routes/dbtemple.js
+++ routes/dbtemple.js
@@ -3,7 +3,7 @@ var express = require('express');
 // acceess mongolab by REST API
 var request = require('request');
 var api_key = "sazUWtXlKYRLcswVw_i6BYQV8Ia6tyE5";
-var api_raw_url = "https://api.mongolab.com/api/1/databases/ibbu-mongo1/collections/dbtest";
+var api_raw_url = "https://api.mongolab.com/api/1/databases/ibbu-mongo1/collections/dbtemples";
 var api_url = api_raw_url+"?apiKey="+api_key;
 var router = express.Router();
 
@@ -17,7 +17,19 @@ router.get('/', function (req, res, next) {
 router.post('/findadrs', function (req, res, next) {
   date = new Date();
   console.log(req.body);
-  res.send("dbtmeple findaddress");
+  words = req.body.words;
+  //res.send("dbtmeple findaddress");
+  q_url = api_raw_url+"?q="+JSON.stringify({"address":{$regex:".*"+words+".*"}})+"&apiKey="+api_key;
+  request( 
+    { 
+      url: q_url, 
+      method: "POST", 
+      json: {"words":words}
+    }, 
+    function (error, response, body) {
+      console.log(body);
+      res.send(body);
+  });
 });
 
 
@@ -25,7 +37,18 @@ router.post('/findadrs', function (req, res, next) {
 router.post('/findkey', function (req, res, next) {
   date = new Date();
   console.log(req.body);
-  res.send("dbtmeple findbykey");
+  //res.send("dbtmeple findbykey");
+  q_url = api_raw_url+"?q="+JSON.stringify({"name":{$regex:".*"+words+".*"}})+"&apiKey="+api_key;
+  request( 
+    { 
+      url: q_url, 
+      method: "POST", 
+      json: {"words":words}
+    }, 
+    function (error, response, body) {
+      console.log(body);
+      res.send(body);
+  });
 });
 
 // POST db find by key area
